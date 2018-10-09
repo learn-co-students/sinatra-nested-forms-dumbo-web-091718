ea7922ec6690d09eb37cf6d384c548dcef95aef5
Brian DiRito <bdirito@gmail.com>
2018-10-09 16:25:57 -0400

    started lab

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do
+      "Welcome to the Nested Forms Lab!"
+      "let's navigate to the '/new'"
+    end
 
+    get "/pirates" do
+      erb :new
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :body
+
+  @@all = []
+
+  def intialize(name, type, body)
+    @name = name
+    @type = type
+    @body = body
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.destroy
+  end
 end
